5545ef6acfb29542e5541056602e84c596508230
Reece <trazhme@gmail.com>
2020-09-23 19:23:14 +0800

    update
    added task manager app

---
+++ task-manager/precache-manifest.26bfa15f0f65b9b10a5f084fda6ae4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c8b18e8aab71cc3afa2fb3d443b17b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265447130eaf0666c74d",
+    "url": "/static/css/main.f21bc2c0.chunk.css"
+  },
+  {
+    "revision": "4a7f9016b415fb371b8d",
+    "url": "/static/js/2.f8eb2f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8eb2f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265447130eaf0666c74d",
+    "url": "/static/js/main.b65121fa.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
